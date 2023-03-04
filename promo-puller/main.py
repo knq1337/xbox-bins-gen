bd416280c0b4954b4a8430304c50e000ba37e65a
knq1337 <99088896+knq1337@users.noreply.github.com>
2023-03-04 19:24:05 +0200

    Update main.py

--- promo-puller/main.py
+++ promo-puller/main.py
@@ -3,20 +3,16 @@ import time
 import json
 
 request_exceptions = (requests.exceptions.ProxyError,requests.exceptions.SSLError,requests.exceptions.Timeout)
-if not json.load(open("config.json"))["proxy"]=="":
-    proxies = {"https":json.load(open("config.json"))["proxy"]}
-else:
-    proxies=None
 
-def remove_content(filename: str, delete_line: str) -> None:
+def remove_content(filename: str, delete_line: str):
         with open(filename, "r+") as io:
-            content = io.readlines()
-            io.seek(0)
+            content = io.readlines() 
             for line in content:
                 if not (delete_line in line):
                     io.write(line)
             io.truncate()
 def puller(ms_creds : str)-> None:
+    global link
     try:
         getAuth = requests.post("http://127.0.0.1:1337/", json={"user": ms_creds.split("|")[0], "pass": ms_creds.split("|")[1]})
         headers = {"authorization":getAuth.text.replace('"','')}
@@ -24,30 +20,34 @@ def puller(ms_creds : str)-> None:
         print("[-] Error sending request to pull promo [Make sure the promo server is running]","y")
         return 
     while True:
+
+        global link
+
         try:
-            getLink = requests.post("https://profile.gamepass.com/v2/offers/47D97C390AAE4D2CA336D2F7C13BA074",headers=headers,proxies=proxies)
+            getLink = requests.post("https://profile.gamepass.com/v2/offers/47D97C390AAE4D2CA336D2F7C13BA074",headers=headers)
+            print(getLink)
             break
         except request_exceptions:
             continue
         except Exception as e:
             print(e,"r")
             return
+    link = getLink.json()["resource"]
     if getLink.status_code==500:
         print("[-] Microsoft internal server error!","c")
-        remove_content("accs.txt",ms_creds)
+        remove_content("emails.txt",ms_creds)
         return
     if getLink.status_code==429:
         print("[!] You are being rate limited!")
         print("[!] Sleeping for 5 minutes...")
-        time.sleep(300)
-    try:
-        link = getLink.json()["resource"]
-    except:
+        time.sleep(300) 
+
+
+    else:
         print(f"[!] Failed to fetch code! Response text : {getLink.text} status code : {getLink.status_code}")
-        remove_content("accs.txt",ms_creds)
+        remove_content("emails.txt",ms_creds)
     print(link)
     open("promos.txt","a").write(link+"\n")
-    remove_content("accs.txt",ms_creds)
 
 for l in open("accs.txt").read().splitlines():
     puller(l)
